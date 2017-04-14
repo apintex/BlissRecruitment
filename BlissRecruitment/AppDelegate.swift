007297f7c4b952a9ef7c6292f809d2a1b7a6863e
António Pinto <apintex@gmail.com>
2017-04-14 08:33:05 +0100

    LoadingScreen Ok and prepareSegue for QuestionsList

--- BlissRecruitment/AppDelegate.swift
+++ BlissRecruitment/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwifterSwift
+import SystemConfiguration
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,6 +30,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(String(describing: url))
+        print(url.absoluteString)
+        print(url.query ?? "")
+        
+        return true
+    }
+
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -52,5 +64,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+
+protocol Utilities {
+}
+
+extension NSObject:Utilities{
+    
+    
+    enum ReachabilityStatus {
+        case notReachable
+        case reachableViaWWAN
+        case reachableViaWiFi
+    }
+    
+    var currentReachabilityStatus: ReachabilityStatus {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return .notReachable
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return .notReachable
+        }
+        
+        if flags.contains(.reachable) == false {
+            // The target host is not reachable.
+            return .notReachable
+        }
+        else if flags.contains(.isWWAN) == true {
+            // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+            return .reachableViaWWAN
+        }
+        else if flags.contains(.connectionRequired) == false {
+            // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+            return .reachableViaWiFi
+        }
+        else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+            // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+            return .reachableViaWiFi
+        }
+        else {
+            return .notReachable
+        }
+    }
+    
 }
 
--- BlissRecruitment/LoadingScreenViewController.swift
+++ BlissRecruitment/LoadingScreenViewController.swift
@@ -9,53 +9,121 @@
 import UIKit
 import KDLoadingView
 import Alamofire
+import SwifterSwift
 
 class LoadingScreenViewController: UIViewController {
 
     @IBOutlet weak var loadingView: KDLoadingView!
+    @IBOutlet weak var lbStatus: UILabel!
+    @IBOutlet weak var btTryConnection: UIButton!
+    
+    var healthStatus:Bool = false {
+        didSet {
+            if healthStatus {
+                //Go to ListaViewController
+                //Stop animation spinner
+                self.loadingView.stopAnimating()
+                self.lbStatus.text = ""
+                self.setHidenStatusOfButton(true)
+                
+                performSegue(withIdentifier: "QuestionsListSegue", sender: nil)
+                
+            }else{
+                //show button to try
+                //Stop animation spinner
+                self.loadingView.stopAnimating()
+                self.lbStatus.text = "Unavailable services!"
+                self.setHidenStatusOfButton(false)
+            }
+            
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        loadingView.startAnimating()
+        checkInternetAndServices()
         
-        let utilityQueue = DispatchQueue.global(qos: .userInitiated)
-        Alamofire.request("https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health").validate().responseJSON(queue: utilityQueue) { response in
+    }
+    
+    func checkInternetAndServices() {
+        
+        setHidenStatusOfButton(true)
+        
+        //Check internet connection
+        if currentReachabilityStatus != .notReachable {
+            //Check health of service
+            callHealthService()
+        }else{
+            //show button to try
+            //Stop animation spinner
+            self.loadingView.stopAnimating()
+            self.lbStatus.text = "Unavailable Internet!"
+            self.setHidenStatusOfButton(false)
+        }
+    }
+
+    func callHealthService() {
+        
+        //Start animation spinner
+        loadingView.startAnimating()
+        lbStatus.text = "Checking internet and services..."
+        
+        //let utilityQueue = DispatchQueue.global(qos: .userInitiated)
+        //Alamofire.request("https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health").validate().responseJSON(queue: utilityQueue) { response in
+        Alamofire.request("https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health").validate().responseJSON() { response in
             
             switch response.result {
             case .success:
-
+                
                 if let parsedData = try? JSONSerialization.jsonObject(with: response.data!) as! [String:Any] {
-                    print(parsedData)
-                    let a = parsedData["status"]
-                    print("Valor de a: \(a ?? "sem valor")")
+                    let status:String = parsedData["status"] as! String
+                    print("Valor status: \(status )")
+                    
+                    if status == "OK" {
+                        self.healthStatus = true
+                    }else{
+                        self.healthStatus = false
+                    }
                 }
                 
             case .failure(let error):
                 print(error)
+                self.healthStatus = false
+                
             }
         }
-        
-        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onBtTryConnectionClick(_ sender: UIButton) {
+        
+        checkInternetAndServices()
     }
     
-
-    /*
+    
+    
+    func setHidenStatusOfButton(_ status: Bool) {
+        btTryConnection?.isHidden = status
+        
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+//        let destinationViewController = segue.destination
+//        if let LoadingScreenViewController = destinationViewController as? LoadingScreenViewController,
+//            let identifier = segue.identifier {
+//                
+//            }
+//        }
     }
-    */
+ 
 
 }
 
