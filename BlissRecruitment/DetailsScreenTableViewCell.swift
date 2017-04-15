caf4302f8824cae3b54c59a53cce8b60dc94abf1
António Pinto <apintex@gmail.com>
2017-04-15 21:09:11 +0100

    implement details form

---
+++ BlissRecruitment/DetailsScreenTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsScreenTableViewCell.swift
+//  BlissRecruitment
+//
+//  Created by António Pinto on 15/04/17.
+//  Copyright © 2017 António Pinto. All rights reserved.
+//
+
+import UIKit
+
+class DetailsScreenTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbChoice: UILabel!
+    @IBOutlet weak var lbVotes: UILabel!
+    @IBOutlet weak var btVote: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func onVoteClick(_ sender: UIButton) {
+        print("increment vote")
+    }
+}
